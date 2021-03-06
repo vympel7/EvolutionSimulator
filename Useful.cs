c8982cf45d32a2948993a6b43a439b37fa6cb99f
vympel7 <vitoaltieri04@gmail.com>
2021-03-06 12:45:53 +0100

    Changed name from GlobalConstants and added method

---
+++ Useful.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Evolution
+{
+    static class Useful
+    {
+        public static Random r = new Random();
+        public const float Divider = 10;
+        public const int Invalid = -1;
+
+        public static int Clamp(int number, int min, int max)
+        {
+            if (number < min) number = min;
+            else if (number > max) number = max - 1;
+            return number;
+        }
+    }
+}
