3621c25e102e03b48302b13a338ca41e75280abc
vympel7 <vitoaltieri04@gmail.com>
2021-03-06 12:44:39 +0100

    Fixed size varying issues

--- World.cs
+++ World.cs
@@ -5,15 +5,16 @@ namespace Evolution
 {
     class World
     {
-        static Random r = new Random();
-
         const int _minSize = 1000;
         const int _maxSize = 10000 + 1;
         const int _minFood = 50;
         const int _maxFood = 1000 + 1;
 
-        public static int Height { get { return r.Next(_minSize, _maxSize); } }
-        public static int Width { get { return r.Next(_minSize, _maxSize); } }
-        public static List<Food> FoodList { get { return new List<Food>(r.Next(_minFood, _maxFood)); } }
+        static int _xSize = Useful.r.Next(_minSize, _maxSize);
+        static int _ySize = Useful.r.Next(_minSize, _maxSize);
+
+        public static int Height { get { return _xSize; } }
+        public static int Width { get { return _ySize; } }
+        public static List<Food> FoodList { get { return new List<Food>(Useful.r.Next(_minFood, _maxFood)); } }
     }
 }
