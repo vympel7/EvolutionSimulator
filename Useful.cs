5e47a14027c37969cec140f9963ed8d2fb40d929
vympel7 <vitoaltieri04@gmail.com>
2021-03-08 17:20:46 +0100

    Added Clamp method

--- Useful.cs
+++ Useful.cs
@@ -8,11 +8,27 @@ namespace Evolution
         public const float Divider = 10;
         public const int Invalid = -1;
 
-        public static int Clamp(int number, int min, int max)
+        /// <summary>
+        /// Ensures a number is between a minimum (inclusive) and a maximum (exclusive) value
+        /// </summary>
+        /// <returns>The number clamped between min and max</returns>
+        /// <param name="number">The number to clamp between min and max</param>
+        /// <param name="min">The minimum value number can have</param>
+        /// <param name="max">The maximum value (-1) number can have</param>
+        public static float Clamp(float number, float min, float max)
         {
             if (number < min) number = min;
-            else if (number > max) number = max - 1;
+            else if (number >= max) number = max - 1;
             return number;
+        }
+
+        public static float Lerp(int x, int minC, int maxC, float a, float b) //fix, now only works for  0 <= x <= 1
+        {
+            float min = a > b ? b : a;
+
+            float d = b - min;
+
+            return min + d * x;
         }
     }
 }
