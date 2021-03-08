270da4ce540792b065bb2e2139949b70e6fd592a
vympel7 <vitoaltieri04@gmail.com>
2021-03-08 17:20:17 +0100

    Added constructor and modified original one

--- Point.cs
+++ Point.cs
@@ -9,10 +9,17 @@ namespace Evolution
         public float X { get { return _x; } set { _x = value; } }
         public float Y { get { return _y; } set { _y = value; } }
 
+
+        public Point()
+        {
+            _x = Useful.r.Next(0, World.Width * 10) / Useful.Divider;
+            _y = Useful.r.Next(0, World.Height * 10) / Useful.Divider;
+        }
+
         public Point(float x, float y)
         {
-            _x = x;
-            _y = y;
+            _x = Useful.Clamp(x, 0, World.Width);
+            _y = Useful.Clamp(y, 0, World.Height);
         }
 
         public void Add(Point other)
@@ -23,7 +30,7 @@ namespace Evolution
 
         public float Manhattan(Point other)
         {
-            return (float) Math.Sqrt(Math.Pow(Math.Abs(_x - other.X), 2) + Math.Pow(Math.Abs(_y - other.Y), 2));
+            return (float)Math.Sqrt(Math.Pow(Math.Abs(_x - other.X), 2) + Math.Pow(Math.Abs(_y - other.Y), 2));
         }
 
         public override string ToString()
